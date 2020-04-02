536e5424ae6ae6673da77300dae7601b957fbad9
Sho Watari <53390550+sho-watari@users.noreply.github.com>
2020-04-02 23:03:53 +0900

    Update stsa_corpus.py

--- STSA/stsa_corpus.py
+++ STSA/stsa_corpus.py
@@ -1,1 +1,158 @@
+import emoji
+import glob
+import MeCab
+import pickle
+import re
 
+from collections import Counter
+from itertools import zip_longest
+
+dir_file = "./twitter"
+
+BOS = "<BOS>"
+EOS = "<EOS>"
+UNK = "<UNK>"
+
+neologd_path = "C:/Users/usr/AppData/Local/Packages/KaliLinux.54290C8133FEE_ey8k8hqnwqnmg/LocalState/rootfs" \
+               "/usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd"
+
+max_seq_len = 70
+
+
+class MeCabTokenizer:
+    def __init__(self):
+        self.tokenizer = MeCab.Tagger("-O wakati -d %s" % neologd_path)
+
+    def tokenize(self, string):
+        return self.tokenizer.parse(string).split(" ")[:-1]
+
+
+def text_cleaning(s):
+    #
+    # twitter specific
+    #
+    s = re.sub(r"http\S+", "", s)  # remove https
+    s = re.sub(r"\@[a-z0-9-_][a-z0-9-_]*", "", s)  # remove @tweet
+    s = re.sub(r"\#.+", "", s)  # remove #tag
+
+    #
+    # normalization
+    #
+    s = re.sub("[˗֊‐‑‒–⁃⁻₋−]+", "-", s)  # normalize hyphens
+    s = re.sub("[﹣－ｰ—―─━ー]+", "ー", s)  # normalize choonpus
+    s = re.sub("[~∼∾〜〰～]", "", s)  # remove tildes
+
+    s = s.lower()  # normalize alphabet to lowercase
+    s = s.translate({ord(x): ord(y) for x, y in zip(  # normalize half-width symbols to full-width symbols
+        "!\"#$%&'()*+,-./:;<=>?@[¥]^_`{|}~｡､･｢｣",
+        "！”＃＄％＆’（）＊＋，－．／：；＜＝＞？＠［￥］＾＿｀｛｜｝〜。、・「」")})
+
+    #
+    # reduce redundancy
+    #
+    s = re.sub(r"！+", "！", s)
+    s = re.sub(r"？？+", "？", s)
+    s = re.sub(r"…+", "…", s)
+    s = re.sub(r"w+w", "。", s)
+
+    #
+    # remove emoji and kaomoji
+    #
+    s = "".join(["" if c in emoji.UNICODE_EMOJI else c for c in s])  # remove emoji
+    s = re.sub(r"（.*）.*", "", s)  # remove kaomoji
+
+    s = re.sub(r"[^a-z0-9ぁ-んァ-ン一-龥、。！？ー…＆／]", "", s)
+
+    return s
+
+
+def create_word2id(captions):
+    counter = Counter()
+    for c in captions:
+        counter.update(c)
+
+    print("Number of total words:", len(counter))
+    word_counts = [x for x in counter.items() if x[1] >= 5]  # less 10 count word is not common word
+    word_counts.sort(key=lambda x: x[1], reverse=True)
+    print("Number of words:", len(word_counts) + 1)  # plus 1 is <UNK>
+    word_list = [x[0] for x in word_counts]
+    word_list.remove(BOS)
+    word_list.remove(EOS)
+    word_list.insert(0, BOS)
+    word_list.insert(0, EOS)
+
+    word_list.append(UNK)
+    word2id = dict([(x, y) for (y, x) in enumerate(word_list)])
+    id2word = dict([(x, y) for (x, y) in enumerate(word_list)])
+
+    return word2id, id2word
+
+
+if __name__ == "__main__":
+    mecab = MeCabTokenizer()
+
+    corpus = []
+    file_list = glob.glob(dir_file + "/*.txt")
+    for file in file_list:
+        with open(file, encoding="UTF-8") as f:
+            courpus += f.readlines()
+    print("Number of tweet and reply:", len(corpus) // 2)
+    print()
+
+    tweet_list = []
+    for t in corpus:
+        t = text_cleaning(t)
+        
+        word_list = mecab.tokenize(t)
+        word_list.insert(0, BOS)
+        word_list.append(EOS)
+
+        tweet_list.append(word_list)
+
+    #
+    # word2id and id2word dictionary
+    #
+    word2id, id2word = create_word2id(tweet_list)
+
+    with open("./word2id.pkl", "wb") as f:
+        pickle.dump(word2id, f)
+    print("\nSaved word2id.pkl")
+
+    with open("./id2word.pkl", "wb") as f:
+        pickle.dump(id2word, f)
+    print("Saved id2word.pkl\n")
+
+    #
+    # create tweet and reply corpus
+    #
+    tweet_reply = []
+    for idx in range(len(tweet_list) // 2):
+        tweet1 = tweet_list[2 * idx]
+        tweet2 = tweet_list[2 * idx + 1]
+
+        if 2 < len(tweet1) < max_seq_len and 2 < len(tweet2) < max_seq_len:
+            tweet = [word2id[w] if w in word2id else word2id[UNK] for w in tweet1]
+            reply = [word2id[w] if w in word2id else word2id[UNK] for w in tweet2]
+            
+            tweet_reply.append([tweet, reply[:-1], reply[1:]])
+    
+    #
+    # tweet, reply, and target
+    #
+    num_samples = 0
+    with open("./tweet_reply_corpus.txt", "w") as ctf_file:
+        for i, (tweet, reply, target) in enumerate(tweet_reply):
+            for (twt, rep, trg) in zip_longest(tweet, reply, target, fillvalue=""):
+                if twt == "":
+                    ctf_file.write("{} |decode {}:1\t|target {}:1\n".format(i, rep, trg))
+                elif rep == "":
+                    ctf_file.write("{} |encode {}:1\n".format(i, twt))
+                else:
+                    ctf_file.write("{} |encode {}:1\t|decode {}:1\t|target {}:1\n".format(i, twt, rep, trg))
+
+            num_samples += 1
+            if num_samples % 10000 == 0:
+                print("Now %d samples..." % num_samples)
+
+    print("\nNumber of samples", num_samples)
+    
